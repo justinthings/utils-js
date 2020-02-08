9885416689c3aecbd4b41f5d2fab4fb6253f18dc
justinthings <justindavidross@gmail.com>
2020-02-08 17:35:59 -0500

    Update throwIfUnsupportedProps.test.js

--- object/throwIfUnsupportedProps.test.js
+++ object/throwIfUnsupportedProps.test.js
@@ -1,1 +1,25 @@
+import { throwIfUnsupportedProps } from './shared'
 
+describe('throwIfUnsupportedProps', () => {
+  it('should NOT throw an error if props are supported', () => {
+    const allowed = { hey: 'yo' }
+    const toCheck = { hey: 'what' }
+    throwIfUnsupportedProps(allowed, toCheck)
+  })
+
+  it('should throw an error if props are not supported', () => {
+    const allowed = { hey: 'yo' }
+    const toCheck = { badProp: 'what' }
+
+    try {
+      throwIfUnsupportedProps(toCheck, allowed)
+    } catch (e) {
+      expect(e).toBeTruthy()
+      expect(e.message).toBeTruthy()
+      const expectedErrorPart = `The following props are not allowed: badProp`
+      expect(e.message.indexOf(expectedErrorPart)).toBeGreaterThan(-1)
+      return
+    }
+    expect('Test did not throw an error').toBe('but it should have thrown an error')
+  })
+})
